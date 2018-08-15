54710d7980dbe30969a29289b5ccbb28812ea65a
Steffen Stolz <stolz1982@gmx.de>
2018-08-15 16:26:20 +0200

    beta version

--- weather.py
+++ weather.py
@@ -2,7 +2,8 @@
 import subprocess
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-from time import sleep
+from time import sleep, gmtime, strftime, localtime
+WORKDIR = "/home/administrator/weather_station/"
 
 command1 = '/home/administrator/weather_station/te923tool-0.6.1/te923con'
 p = subprocess.Popen(command1, stdout=subprocess.PIPE, shell=True)
@@ -39,20 +40,42 @@ p_status = p.wait()
 
 #1534271542:26.05:56:21.00:77:i:i:i:i:i:i:i:i:1006.6:i:5:0:11:1.7:1.4:19.2:76
 DT,TIN,HIN,TOUT,HOUT,T2,H2,T3,H3,T4,H4,T5,H5,PRESS,UV,FC,STORM,WD,WS,WG,WC,RC = output.split(":")
-TIN = float(TIN) #print "TIN: ", TIN
-HIN = float(HIN) #print "HIN0: ", HIN
-TOUT = float(TOUT) #print "TOUT: ", TOUT
-HOUT = float(HOUT) #print "HOUT: ", HOUT
-PRESS = float(PRESS) #print "PRESS: ", PRESS
-WD = float(WD) #print "WIND DIRECTION: ", WD
-WS = float(WS) #print "WIND SPEED: ", float(WS)
-WG = float(WG)*3.6 #print "WIND GUST: ", WG
-WC = float(WC) #print "WIND CHILL: ", WC
-RC = float(RC) #print "RAIN COUNTER: ", RC
+TIN = float(TIN)
+HIN = float(HIN) 
+TOUT = float(TOUT) 
+HOUT = float(HOUT)
+PRESS = float(PRESS)
+FC = float(FC)
+
+if FC == 0:
+	FC_STR = "Heavy Snow"
+elif FC == 1:
+	FC_STR = "Little Snow"
+elif FC == 2:
+	FC_STR = "Heavy Rain"
+elif FC == 3:
+	FC_STR = "Little Rain"
+elif FC == 4:
+	FC_STR = "Cloudy"
+elif FC == 5:
+	FC_STR = "Some Clouds"
+else:
+	FC_STR = "Sunny"
+
+WD = float(WD)*22.5 
+WS = float(WS)*3.6
+WG = float(WG)*3.6
+WC = float(WC)
+RC = float(RC)
 
 
-#scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-#creds = ServiceAccountCredentials.from_json_keyfile_name('rpi_googlesheet.json', scope) # rpi_$
-#client = gspread.authorize(creds)
-#sheet = client.open('Remote Monitoring')  # Google Spreadsheet name 'Remote Monitoring'
-#worksheet = sheet.worksheet("Weather") # Worksheet name 'Weather'
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('/home/administrator/weather_station/weather_sheet.json', scope) # rpi_$
+client = gspread.authorize(creds)
+sheet = client.open('Remote Monitoring')  # Google Spreadsheet name 'Remote Monitoring'
+worksheet = sheet.worksheet("Weather") # Worksheet name 'Weather'
+now = strftime("%d %b %Y %H:%M:%S", localtime()) # get current timestamp
+
+data_set = [now, TIN, HIN, TOUT, HOUT, PRESS, FC_STR, WD, WS, WG, WC, RC]      # data package to be sent to Google sheet
+worksheet.append_row(data_set)  # append the targetted google sheet with the above data set
+
